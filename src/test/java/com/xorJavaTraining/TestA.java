618066733309db0f911b04ce4008b49254384bf2
Brij Bhan Singh <Singh_Br@India.XoriantCorp.com>
2020-11-27 15:36:49 +0530

    First Commit

---
+++ src/test/java/com/xorJavaTraining/TestA.java
@@ -0,0 +1,12 @@
+package com.xorJavaTraining;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestA {
+
+	@Test
+	public void test1() {
+		Assert.assertEquals("TESTA", "TESTA");
+	}
+}
---
+++ src/test/java/com/xorJavaTraining/TestB.java
@@ -0,0 +1,18 @@
+package com.xorJavaTraining;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestB {
+	
+	@Test
+	public void test1() {
+		Assert.assertEquals("TESTA", "TESTA");
+	}
+	
+	@Test
+	public void test2() {
+		Assert.assertEquals("TESTA", "TESTA");
+	}
+
+}
